35394d934d220dcf18d96ca739cb72bb7fbb37d6
SakshiKoli-CS <sakshi.koli@contentstack.com>
2025-09-18 09:03:58 +0530

    add logs

--- apps/blog/src/lib/contentstack.ts
+++ apps/blog/src/lib/contentstack.ts
@@ -1,5 +1,11 @@
 import * as contentstack from "contentstack";
 
+
+console.log('Environment variables check:');
+console.log('API_KEY exists:', !!process.env.CONTENTSTACK_API_KEY);
+console.log('DELIVERY_TOKEN exists:', !!process.env.CONTENTSTACK_DELIVERY_TOKEN);
+console.log('ENVIRONMENT exists:', !!process.env.CONTENTSTACK_ENVIRONMENT);
+
 const stack = contentstack.Stack({
   api_key: process.env.CONTENTSTACK_API_KEY as string,
   delivery_token: process.env.CONTENTSTACK_DELIVERY_TOKEN as string,
