28a6a49de5fbe509031bae9438d810746f1eaadb
SakshiKoli-CS <sakshi.koli@contentstack.com>
2025-10-08 10:25:52 +0530

    remove rewrite

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -285,12 +285,7 @@ export default async function handler(request, context) {
       source: "/blog",
       destination: "/blog/latest",
       onlyOnProd: true,
-    },
-    {
-      source: "/blog",
-      destination: "/404",
-      onlyOnPreview: true,
-    },
+    }
   ];
 
   const rewriteResponse = await processRewrites(rewrites, request);
